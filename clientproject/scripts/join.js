67eeeb08c1dbdbc256584466228a3f351241db3a
Hunter1580 <hunter.starets@gmail.com>
2023-05-07 19:29:59 -0400

    jquery plugins

--- clientproject/scripts/join.js
+++ clientproject/scripts/join.js
@@ -1,29 +1,51 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const joinForm = document.getElementById("joinForm");
-
-  joinForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-    if (validateForm()) {
-      // Process the form data
+$(document).ready(function () {
+  const joinForm = $("#joinForm");
+  joinForm.validate({
+    rules: {
+      firstName: {
+        required: true,
+      },
+      lastName: {
+        required: true,
+      },
+      email: {
+        required: true,
+        email: true,
+      },
+      phone: {
+        required: true,
+      },
+      dob: {
+        required: true,
+        date: true,
+      },
+    },
+    messages: {
+      firstName: {
+        required: "Please enter your first name",
+      },
+      lastName: {
+        required: "Please enter your last name",
+      },
+      email: {
+        required: "Please enter your email address",
+        email: "Please enter a valid email address",
+      },
+      phone: {
+        required: "Please enter your phone number",
+      },
+      dob: {
+        required: "Please enter your date of birth",
+        date: "Please enter a valid date",
+      },
+    },
+    errorPlacement: function (error, element) {
+      const errorElement = $("#" + element.attr("name") + "Error");
+      error.appendTo(errorElement);
+      errorElement.addClass("active");
+    },
+    submitHandler: function () {
       console.log("Form submitted");
-    }
+    },
   });
-
-  function validateForm() {
-    const inputs = joinForm.elements;
-    let isValid = true;
-
-    for (const input of inputs) {
-      const errorElement = document.getElementById(input.name + "Error");
-      if (input.checkValidity()) {
-        errorElement.classList.remove("active");
-      } else {
-        errorElement.textContent = input.validationMessage;
-        errorElement.classList.add("active");
-        isValid = false;
-      }
-    }
-
-    return isValid;
-  }
 });
